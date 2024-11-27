67fad1013b3f97ad84f47723afd7543103360c7b
Jonathan Robert <jr@MacBook-Pro-de-JR.local>
2024-11-27 16:46:47 +0100

    update

--- auth.js
+++ auth.js
@@ -102,6 +102,7 @@ async function reBookIds(cookies) {
       keuringsTypeId,
       oldReservationId
   };
+  console.log("IDS = "+ids[0]+" - "+ids[1]+" - "+ids[2]+" - "+ids[3]);
   return (ids);
   } catch (error) {
       console.error('Erreur lors de la récupération des réservations :', error.message);
