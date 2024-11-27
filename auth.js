293bca9010ddde78d5c457f458b4e37a70f0da1b
Jonathan Robert <jr@MacBook-Pro-de-JR.local>
2024-11-27 18:44:48 +0100

    update

--- auth.js
+++ auth.js
@@ -111,6 +111,7 @@ async function reBookIds(cookies) {
 
 async function getHaren(cookies, ids) {
   try {
+      const qs = require('querystring');
       const headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
         'Accept-Encoding': 'gzip, deflate, br, zstd',
