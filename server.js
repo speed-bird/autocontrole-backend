17e4161f52e431bb8724aa0efbddda963637492d
Jonathan Robert <jr@mbp-de-jr.home>
2024-11-28 14:43:30 +0100

    update

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.post('/find-slots', async (req, res) => {
     console.log("Reservation selected = " + selectedReservation);
     const ids = await reBookIds(cookies);
     const haren = await getHaren(cookies, ids);
-    res.json({ message: 'Nouvelle action traitée', haren });
+    res.json({ message: 'Nouvelle action traitée', haren: haren });
   }
   catch (error) {
     console.error('Erreur lors de la connexion :', error.message);
