e08ff56faa45921c821bdbf1c4f263d0faa73564
Jonathan Robert <robert_jonathan@hotmail.com>
2024-12-02 21:55:14 +0100

    update

--- auth.js
+++ auth.js
@@ -14,11 +14,9 @@ async function auth(username, password) {
       maxRedirects: 0,
       validateStatus: (status) => status <= 302,
     });
-
     let cookies = [];
     const loginPage = await instance.get('/login.aspx');
     const $ = cheerio.load(loginPage.data);
-
     const loginResponse = await instance.post(
       '/login.aspx',
       new URLSearchParams({
@@ -38,37 +36,34 @@ async function auth(username, password) {
         },
       }
     );
-
-    const loginCookies = (loginResponse.headers['set-cookie'] || []).map((cookie) => cookie.split(';')[0]);
+    const loginCookies = (loginResponse.headers['set-cookie'] || []).map(cookie => cookie.split(';')[0]);
     cookies = cookies.concat(loginCookies);
-    return cookies;
+    return (cookies);
   } catch (error) {
-    console.error('Erreur :', error.message);
+      console.error('Erreur :', error.message);
     throw error;
   }
 }
 
 async function getMain(cookies) {
   const mainURL = 'https://planning.autocontrole.be/Reservaties/ReservatieOverzicht.aspx';
-  const mainPage = await axios.get(mainURL, { headers: { Cookie: cookies.join('; ') } });
-  return mainPage;
+    const mainPage = await axios.get( mainURL, { headers: { Cookie: cookies.join('; ') } });
+    return (mainPage);
 }
 
-function getBookings(mainPage) {
+function getBookings (mainPage) {
   const bookings = [];
-  const $ = cheerio.load(mainPage.data);
-
+  let $ = cheerio.load(mainPage.data);
   $('a[id*="lbRebook"]').each((index, element) => {
-    bookings.push({
-      date: $(element).closest('tr').find('td:nth-child(3)').text().trim(),
-      time: $(element).closest('tr').find('td:nth-child(4)').text().trim(),
-      location: $(element).closest('tr').find('td:nth-child(5)').text().trim(),
-      plate: $(element).closest('tr').find('td:nth-child(6)').text().trim(),
-      model: $(element).closest('tr').find('td:nth-child(7)').text().trim(),
-    });
-  });
-
-  return bookings;
+    bookings.push(
+      { date: $(element).closest("tr").find("td:nth-child(3)").text().trim(),
+        time: $(element).closest("tr").find("td:nth-child(4)").text().trim(),
+        location: $(element).closest("tr").find("td:nth-child(5)").text().trim(),
+        plate: $(element).closest("tr").find("td:nth-child(6)").text().trim(),
+        model: $(element).closest("tr").find("td:nth-child(7)").text().trim()
+    })}
+  );
+  return (bookings);
 }
 
 async function getIds(cookies) {
@@ -76,7 +71,6 @@ async function getIds(cookies) {
     const resaURL = 'https://planning.autocontrole.be/Reservaties/ReservatieOverzicht.aspx';
     const resaPage = await axios.get(resaURL, { headers: { Cookie: cookies.join('; ') } });
     let $ = cheerio.load(resaPage.data);
-
     const response = await axios.post(
       resaURL,
       new URLSearchParams({
@@ -86,50 +80,58 @@ async function getIds(cookies) {
         __VIEWSTATEGENERATOR: $('input[name="__VIEWSTATEGENERATOR"]').val(),
         __EVENTVALIDATION: $('input[name="__EVENTVALIDATION"]').val(),
       }),
-      {
-        headers: {
-          Cookie: cookies.join('; '),
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      }
+      { headers: { Cookie: cookies.join('; '), 'Content-Type': 'application/x-www-form-urlencoded' } }
     );
-
     $ = cheerio.load(response.data);
+   // console.log("HTML = ", response.data);
     const formAction = $('form').attr('action');
     const urlParams = new URLSearchParams(formAction.split('?')[1]);
-
-    return {
-      voertuigId: urlParams.get('VoertuigId'),
-      klantId: urlParams.get('KlantId'),
-      keuringsTypeId: urlParams.get('KeuringsTypeId'),
-      oldReservationId: urlParams.get('oldReservationId'),
-    };
+    const voertuigId = urlParams.get('VoertuigId');
+    const klantId = urlParams.get('KlantId');
+    const keuringsTypeId = urlParams.get('KeuringsTypeId');
+    const oldReservationId = urlParams.get('oldReservationId');
+    
+    const ids = {
+      voertuigId,
+      klantId,
+      keuringsTypeId,
+      oldReservationId
+  };
+  return (ids);
   } catch (error) {
-    console.error('Erreur lors de la récupération des réservations :', error.message);
+      console.error('Erreur lors de la récupération des réservations :', error.message);
     throw error;
   }
 }
 
 async function getSlots(cookies, ids) {
-  const resaUrl = 'https://planning.autocontrole.be/Reservaties/NieuwAutokeuringReservatie.aspx';
-  const resaPage = await axios.get(resaUrl, { headers: { Cookie: cookies.join('; ') } });
-  let $ = cheerio.load(resaPage.data);
 
+  const resaUrl =
+  'https://planning.autocontrole.be/Reservaties/NieuwAutokeuringReservatie.aspx?VoertuigId=16e825e6-e99c-41d2-8461-4e1460dc080b&KlantId=9b495d05-bbf7-4c4d-8bc9-bdb2941f5ef2&KeuringsTypeId=4fefac0f-e376-4c11-815b-59a137c3c88b';
+  const resaPage = await axios.get(reservationUrl, { headers: { Cookie: cookies.join('; ') } });
+  let $ = cheerio.load(resaPage.data);
   const viewStateResa = $('input[name="__VIEWSTATE"]').val();
   const viewStateGeneratorResa = $('input[name="__VIEWSTATEGENERATOR"]').val();
   const eventValidationResa = $('input[name="__EVENTVALIDATION"]').val();
-
   const stations = [
-    { name: 'Schaerbeek', target: 'ctl00$MainContent$rblStation$0', id: 'FABB7EFC-F207-4043-A39D-40F24D800C93' },
-    { name: 'Haren', target: 'ctl00$MainContent$rblStation$1', id: '289340F7-3DF5-43AD-AFB7-71E4A27FE94D' },
+    {
+      name: 'Schaerbeek',
+      target: 'ctl00$MainContent$rblStation$0',
+      id: 'FABB7EFC-F207-4043-A39D-40F24D800C93',
+    },
+    {
+      name: 'Haren',
+      target: 'ctl00$MainContent$rblStation$1',
+      id: '289340F7-3DF5-43AD-AFB7-71E4A27FE94D',
+    },
   ];
-
   const slots = {};
   for (const station of stations) {
     console.log(`\n--- Vérification pour la station : ${station.name} ---`);
-
-    if (!slots[station.name]) slots[station.name] = [];
-
+    if (!slots[station.name]) {
+      slots[station.name] = []; // Initialisez un tableau vide si nécessaire
+    }
+     
     const params = new URLSearchParams({
       __EVENTTARGET: station.target,
       __EVENTARGUMENT: '',
@@ -137,12 +139,10 @@ async function getSlots(cookies, ids) {
       __VIEWSTATEGENERATOR: viewStateGeneratorResa,
       __EVENTVALIDATION: eventValidationResa,
       ctl00$MainContent$rblStation: station.id,
-    });
-
+    }); 
     const resaResponse = await axios.post(resaUrl, params, { headers: { Cookie: cookies.join('; ') } });
     const pageHTML = resaResponse.data;
-    $ = cheerio.load(pageHTML);
-
+    const $ = cheerio.load(pageHTML);
     const tijdstipIds = [
       'ctl00_MainContent_rblTijdstip1',
       'ctl00_MainContent_rblTijdstip2',
@@ -152,19 +152,58 @@ async function getSlots(cookies, ids) {
       'ctl00_MainContent_rblTijdstip6',
       'ctl00_MainContent_rblTijdstip7',
     ];
-
     tijdstipIds.forEach((tijdstipId) => {
       const tijdstipSpan = $(`#${tijdstipId}`);
       if (tijdstipSpan.length) {
         const date = tijdstipSpan.attr('title') || 'Date inconnue';
         const times = tijdstipSpan.text().trim().split(/(?=\d{2}:\d{2})/);
         times.forEach((time) => {
-          if (time) slots[station.name].push({ date, time });
+          if (time) {
+            slots[station.name].push({ date, time });
+          }
         });
       }
     });
+    const maxAttempts = 3;
+    let attempts = 0;
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+    while (attempts < maxAttempts) {
+      console.log(`Tentative ${attempts + 1} pour la station ${station.name}`);
+      resaResponse = await axios.post(resaURL, 
+        new URLSearchParams({
+          __EVENTTARGET: $('#ctl00_MainContent_lbDatumVolgende'),
+          __EVENTARGUMENT: '',
+          __VIEWSTATE: $('input[name="__VIEWSTATE"]').val(),
+          __VIEWSTATEGENERATOR: $('input[name="__VIEWSTATEGENERATOR"]').val(),
+          __EVENTVALIDATION: $('input[name="__EVENTVALIDATION"]').val(),
+        }),
+        { headers: { Cookie: cookies.join('; ') } });
+      pageHTML = resaResponse.data;
+      $ = cheerio.load(pageHTML);
+      tijdstipIds.forEach((tijdstipId) => {
+        const tijdstipSpan = $(`#${tijdstipId}`);
+        if (tijdstipSpan.length) {
+          const date = tijdstipSpan.attr('title') || 'Date inconnue';
+          const times = tijdstipSpan.text().trim().split(/(?=\d{2}:\d{2})/);
+  
+          times.forEach((time) => {
+            if (time) {
+              slots[station.name].push({ date, time });
+            }
+          });
+        }
+      });
+      if ($('body').text().includes('Texte attendu')) {
+        console.log('Texte trouvé, arrêt des vérifications.');
+        break;
+      }
+      await delay(2000);
+      attempts++;
+      if (attempts === maxAttempts) {
+        console.log("Nombre maximum de tentatives atteint.");
+      }
+    }
   }
-
   return slots;
 }
 
