3959b1cac1de1d98513ad9f9047d1ea42d6cdcca
Jonathan Robert <robert_jonathan@hotmail.com>
2024-12-03 22:37:35 +0100

    update

--- auth.js
+++ auth.js
@@ -174,7 +174,6 @@ async function getSlots(cookies, ids) {
           __VIEWSTATE: $('input[name="__VIEWSTATE"]').val(),
           __VIEWSTATEGENERATOR: $('input[name="__VIEWSTATEGENERATOR"]').val(),
           __EVENTVALIDATION: $('input[name="__EVENTVALIDATION"]').val(),
-      
         }),
         { headers: { Cookie: cookies.join('; ') } });
       pageHTML = resaResponse.data;
