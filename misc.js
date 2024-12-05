dfab30033a36ae2cf9192561581732c6f34f2870
Jonathan Robert <robert_jonathan@hotmail.com>
2024-12-05 16:59:44 +0100

    update

--- misc.js
+++ misc.js
@@ -13,6 +13,9 @@ function getNextMondayDate(dateInput) {
     let day = date.getDate().toString().padStart(2, '0');
     let month = (date.getMonth() + 1).toString().padStart(2, '0');
     let year = date.getFullYear();
+  
+    return `${day}/${month}/${year}`;
+    
 }
 
 function getTodayFormatted() {
