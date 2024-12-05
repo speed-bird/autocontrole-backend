7c7871cb990ffd45ec7f14d975274dd0f9d26508
Jonathan Robert <robert_jonathan@hotmail.com>
2024-12-05 15:49:12 +0100

    update

--- misc.js
+++ misc.js
@@ -1,21 +1,18 @@
 function getNextMondayDate(dateInput) {
-    // Obtenir la date actuelle
-    let date = new Date(dateInput);
-  
+    let parts = dateInput.split('/');
+    let date = new Date(parts[2], parts[1] - 1, parts[0]); // (year, month, day)
+
     // Calculer le nombre de jours jusqu'au lundi suivant
     let daysToAdd = (1 - date.getDay() + 7) % 7;
-    if (daysToAdd === 0) daysToAdd = 7; // Si aujourd'hui est déjà lundi, on ajoute 7 jours pour obtenir le lundi suivant
-  
+    if (daysToAdd === 0) daysToAdd = 7; // Si la date est déjà un lundi, on ajoute 7 jours pour obtenir le lundi suivant
+
     // Ajouter les jours nécessaires pour atteindre le lundi suivant
     date.setDate(date.getDate() + daysToAdd);
-  
+
     // Formater la date en "dd/mm/yyyy"
     let day = date.getDate().toString().padStart(2, '0');
     let month = (date.getMonth() + 1).toString().padStart(2, '0');
     let year = date.getFullYear();
-  
-    return `${day}/${month}/${year}`;
-    
 }
 
 function getTodayFormatted() {
