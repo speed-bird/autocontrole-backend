cdff46a9a32facd65f4c0819b0800ba60517d1f9
Jonathan Robert <robert_jonathan@hotmail.com>
2024-12-05 15:37:26 +0100

    update

--- misc.js
+++ misc.js
@@ -3,7 +3,7 @@ function getNextMondayDate(dateInput) {
     let date = new Date(dateInput);
   
     // Calculer le nombre de jours jusqu'au lundi suivant
-    let daysToAdd = (1 - today.getDay() + 7) % 7;
+    let daysToAdd = (1 - date.getDay() + 7) % 7;
     if (daysToAdd === 0) daysToAdd = 7; // Si aujourd'hui est déjà lundi, on ajoute 7 jours pour obtenir le lundi suivant
   
     // Ajouter les jours nécessaires pour atteindre le lundi suivant
